edcbfebf328eaef9c1a23c3bca38122504eb3cd7
Schamir Poliard <99343841+schamiir@users.noreply.github.com>
2023-02-17 11:05:01 -0500

    code challenge solution

--- src/main/java/tech/strategio/E01.java
+++ src/main/java/tech/strategio/E01.java
@@ -4,7 +4,12 @@ import java.util.*;
 
 public class E01 {
     public int getNthFib(int n) {
-        // FIXME
-        return -1;
+        if (n == 1) {
+            return 0;
+        } else if (n == 2) {
+            return 1;
+        } else {
+            return getNthFib(n - 1) + getNthFib(n - 2);
+        }
     }
 }
